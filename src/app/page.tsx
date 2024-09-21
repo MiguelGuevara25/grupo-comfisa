8a41525e5260a7ea18a2412d2a3d4b831d98c24b
MiguelGuevara25 <miguel_g1789@hotmail.com>
2024-09-21 10:38:11 -0500

    instal dependency for maps in contact

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-import Catalogo from "@/components/Catalogo";
-import Locales from "@/components/Locales";
-import Prueba from "@/components/Prueba";
+import Catalogo from "@/components/home/Catalogo";
+import Locales from "@/components/home/Locales";
+import Prueba from "@/components/home/Prueba";
 export default function Home() {
   return (
     <main>
